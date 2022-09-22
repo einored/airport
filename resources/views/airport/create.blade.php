85bae1e6b8d275d65e95cfc9a1460baa80fc5355
Einoras <58789065+einored@users.noreply.github.com>
2022-09-22 15:26:42 +0300

    map update

--- resources/views/airport/create.blade.php
+++ resources/views/airport/create.blade.php
@@ -1,6 +1,42 @@
 @extends('layouts.app')
 @section('title', 'Create')
 
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+    var map;
+
+    function initialize() {
+        latitude = 50;
+        longitude = 20;
+        latlng = new google.maps.LatLng(latitude, longitude);
+
+        var mapOpt = {
+            center: latlng
+            , zoom: 3
+            , zoomControl: true
+            , scaleControl: true
+            , mapTypeId: google.maps.MapTypeId.HYBRID
+        , };
+
+        map = new google.maps.Map(document.getElementById("googleMap"), mapOpt);
+
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(latitude, longitude)
+            , draggable: false
+            , animation: true
+            , map: map
+        })
+        marker.bindTo('position', map, 'center');
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    function setLatLon(){
+        document.getElementById("latitude").value = map.getBounds().getNorthEast().lat();
+        document.getElementById("longitude").value = map.getBounds().getNorthEast().lng();
+    }
+
+</script>
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -22,9 +58,11 @@
                                 @endforeach
                             </select>
                             <li>Airport latitude</li>
-                            <input type="text" class="form-control" name="create_airport_latitude" />                            
+                            <input type="text" class="form-control" id="latitude" name="create_airport_latitude" />                            
                             <li>Airport longitude</li>
-                            <input type="text" class="form-control" name="create_airport_longitude" />
+                            <input type="text" class="form-control" id="longitude" name="create_airport_longitude" />
+                            <div id="googleMap" style="width:100%;height:350px;" onclick="setLatLon()"></div>
+
                             <li>Airline(-s)</li>
                             <select class="form-control" name="create_airport_airline_id" required focus>
                                 <option value="" disabled selected>Please select airline</option>
@@ -37,7 +75,6 @@
                             <button type="submit" class="button-top-margin btn btn-success btn-sm">Create</button>
                         </form>
                     </ul>
-
                 </div>
             </div>
         </div>
--- resources/views/airport/edit.blade.php
+++ resources/views/airport/edit.blade.php
@@ -1,6 +1,42 @@
 @extends('layouts.app')
 @section('title', 'Edit')
 
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+    var map;
+
+    function initialize() {
+        latitude = <?php echo $airport->latitude ?>;
+        longitude = <?php echo $airport->longitude ?>;
+        latlng = new google.maps.LatLng(latitude, longitude);
+
+        var mapOpt = {
+            center: latlng
+            , zoom: 5
+            , zoomControl: true
+            , scaleControl: true
+            , mapTypeId: google.maps.MapTypeId.HYBRID
+        , };
+
+        map = new google.maps.Map(document.getElementById("googleMap"), mapOpt);
+
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(latitude, longitude)
+            , draggable: false
+            , animation: true
+            , map: map
+        })
+        marker.bindTo('position', map, 'center');
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+    function setLatLon(){
+        document.getElementById("latitude").value = map.getBounds().getNorthEast().lat();
+        document.getElementById("longitude").value = map.getBounds().getNorthEast().lng();
+    }
+
+</script>
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
@@ -21,9 +57,10 @@
                                 @endforeach
                             </select>
                             <li>Airport latitude</li>
-                            <input type="text" class="form-control" name="airport_latitude" value="{{old('airport_latitude', $airport->latitude)}}" /> 
+                            <input type="text" class="form-control" id="latitude" name="airport_latitude" value="{{old('airport_latitude', $airport->latitude)}}" /> 
                             <li>Airport longitude</li>
-                            <input type="text" class="form-control" name="airport_longitude" value="{{old('airport_longitude', $airport->longitude)}}" />   
+                            <input type="text" class="form-control"  id="longitude" name="airport_longitude" value="{{old('airport_longitude', $airport->longitude)}}" />   
+                            <div id="googleMap" style="width:100%;height:350px;" onclick="setLatLon()"></div>
                             <li>Airline(-s)</li>
                             <select class="form-control" name="airport_airline_id" required focus>
                                 @foreach($airlines as $airline)
@@ -34,6 +71,7 @@
                             @method('put')
                             <button type="submit" class="button-top-margin btn btn-success btn-sm">Edit</button>
                         </form>
+                        
                     </ul>
                 </div>
             </div>
